245bf03687e4115618070bf49abbb48c188d30da
小北 <huting10830@163.com>
2017-09-27 11:10:11 +0800

    timer

---
+++ src/views/components/timer.js
@@ -0,0 +1,22 @@
+import React,{ Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View
+} from 'react-native'
+
+export default class timer extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      time:1234567;
+    }
+  }
+  render(){
+    return (
+      <View>
+
+      </View>
+    )
+  }
+}
