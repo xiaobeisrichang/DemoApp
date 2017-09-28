8f83a52e0b5675991f41607a59907b8d074da35b
huting <mikasa.hu@bestwehotel.com>
2017-09-28 11:06:12 +0800

    倒计时增加app状态判断，app到后台则clear掉，active时拉起

--- src/views/components/timer.js
+++ src/views/components/timer.js
@@ -8,7 +8,7 @@ export default class timer extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      timeStr:'',
+      timeStr: '',
       startTime: this.props.startTime
         ? this.props.startTime
         : moment().toDate().getTime(), //如果为空，选择当前时间，为精确到毫秒的时间戳
@@ -24,8 +24,9 @@ export default class timer extends Component {
     this._index = 0;
   }
   componentWillMount() {
+    this.countTime();
     //监听状态改变事件
-    AppState.addEventListener('change', this.handleAppStateChange);
+    AppState.addEventListener('change', this.handleAppStateChange.bind(this));
     //监听内存报警事件
     AppState.addEventListener('memoryWarning', function() {
       console.log("内存报警....");
@@ -34,7 +35,7 @@ export default class timer extends Component {
 
   componentWillUnmount() {
     //删除状态改变事件监听
-    AppState.removeEventListener('change', this.handleAppStateChange);
+    AppState.removeEventListener('change', this.handleAppStateChange.bind(this));
   }
   render() {
     return (
@@ -45,12 +46,34 @@ export default class timer extends Component {
   }
 
   //状态改变响应
-  handleAppStateChange(appState) {
-    alert('当前状态为:' + appState);
+  handleAppStateChange() {
+    switch (AppState.currentState) {
+      case 'active': //前台运行中
+        this.setState({startTime: moment().toDate().getTime()});
+        this.countTime();
+        break;
+      case 'background': //后台运行中
+        this._time && clearInterval(this._time);
+        break;
+      case 'Inactive': //运行过度状态
+        this.setState({startTime: moment().toDate().getTime()});
+        this.countTime();
+        break;
+    }
   }
 
-
-
+  countTime() {
+    this._time = setInterval(() => {
+      this.setState({
+        startTime: this.state.startTime + 1000
+      })
+      if (this.state.startTime <= 0) {
+        this._time && clearInterval(this._time);
+      } else {
+        this.count(this.state.startTime, this.state.endTime);
+      }
+    }, 1000)
+  }
 
   count(start, end) {
     let timeLine = (end - start) / 1000;
@@ -58,12 +81,21 @@ export default class timer extends Component {
     let hour = Math.floor((timeLine - (day * 24 * 60 * 60)) / (60 * 60)) > 9
       ? Math.floor((timeLine - (day * 24 * 60 * 60)) / (60 * 60))
       : "0" + Math.floor((timeLine - (day * 24 * 60 * 60)) / (60 * 60));
-    let min = Math.floor((timeLine - day * 24 * 60 * 60 - hour * 60 * 60) / (60));
-    let sec = Math.floor(timeLine - day * 24 * 60 * 60 - hour * 60 * 60 - min * 60);
+    let min = Math.floor((timeLine - day * 24 * 60 * 60 - hour * 60 * 60) / (60)) > 9
+      ? Math.floor((timeLine - day * 24 * 60 * 60 - hour * 60 * 60) / (60))
+      : '0' + Math.floor((timeLine - day * 24 * 60 * 60 - hour * 60 * 60) / (60));
+    let sec = Math.floor(timeLine - day * 24 * 60 * 60 - hour * 60 * 60 - min * 60) > 9
+      ? Math.floor(timeLine - day * 24 * 60 * 60 - hour * 60 * 60 - min * 60)
+      : '0' + Math.floor(timeLine - day * 24 * 60 * 60 - hour * 60 * 60 - min * 60);
+
     if (day > 0) {
-      this.setState({"timeStr":day + "天 " + hour + ":" + min + ":" + sec});
+      this.setState({
+        "timeStr": day + "天 " + hour + ":" + min + ":" + sec
+      });
     } else {
-      this.setState({"timeStr":hour + ":" + min + ":" + sec});
+      this.setState({
+        "timeStr": hour + ":" + min + ":" + sec
+      });
     }
 
   }
